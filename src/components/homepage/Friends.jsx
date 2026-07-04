dfdf4b0fed381de577679f1dd64fc065aad63197
sanjidaj <sanjidajahan341@gmail.com>
2026-07-04 14:34:43 +0600

    Implement data fetching and added footer

--- src/components/homepage/Friends.jsx
+++ src/components/homepage/Friends.jsx
@@ -1,13 +1,65 @@
+import { use } from "react";
 
-
+const friendsPromise = fetch('/friends.json')
+ .then(res => res.json());
 const Friends = () => {
+    const friends = use(friendsPromise);
+    console.log(friends);
     return (
         <>
-          <div className="mt-8">
             <div>
-                <h2 className="text-[24px] font-semibold">Your Friends</h2>
-            </div>
+                <div className="mt-8">
+                    <h2 className="text-2xl font-bold mb-6">Your Friends</h2>
 
+                    <div className="grid grid-cols-4 gap-4">
+                        {friends.map((friend) => (
+                            <div
+                                key={friend.id}
+                                className="bg-white rounded-xl shadow-md p-4 flex flex-col items-center text-center mb-8"
+                            >
+                                <img
+                                    src={friend.picture}
+                                    alt={friend.name}
+                                    className="w-20 h-20 rounded-full object-cover"
+                                />
+
+                                <h3 className="text-xl font-semibold mt-2">
+                                    {friend.name}
+                                </h3>
+
+                                <p className="text-[#64748B]  text-sm mt-1">
+                                    {friend.days_since_contact}d ago
+                                </p>
+
+                                <div className="flex gap-1 mt-2 flex-wrap justify-center">
+                                    {friend.tags.map((tag) => (
+                                        <span
+                                            key={tag}
+                                            className="bg-green-100 text-green-700 text-xs px-2 py-1 rounded-full uppercase"
+                                        >
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </div>
+
+                                <span
+                                    className={`mt-4 px-4 py-1 rounded-full text-white text-sm ${friend.status === "overdue"
+                                            ? "bg-red-500"
+                                            : friend.status === "almost due"
+                                                ? "bg-yellow-500"
+                                                : "bg-green-800"
+                                        }`}
+                                >
+                                    {friend.status === "almost due"
+                                        ? "Almost Due"
+                                        : friend.status === "on-track"
+                                            ? "On-Track"
+                                            : "Overdue"}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
           </div>
         </>
     );
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,8 +2,29 @@
 
 const Footer = () => {
     return (
-        <div>
-            footer
+        <div className="bg-[#244D3F] px-10 py-8">
+            <div className="text-center space-y-4">
+                <img className="inline-flex mt-10" src="/src/assets/logo-xl.png" alt="" />
+                 <p className="text-[#FAFAFA]">Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+                 <h4 className="text-[#FAFAFA] font-medium">Social Links</h4>
+                 <div className="flex justify-center gap-3">
+                    <a href=""><img src="/src/assets/instagram.png" alt="" /></a>
+                    <a href=""><img src="/src/assets/facebook.png" alt="" /></a>
+                    <a href=""><img src="/src/assets/twitter.png" alt="" /></a>
+
+                 </div>
+            </div>
+             <div className="divider text-[#1A8862]"></div>
+            <div className="flex justify-around">
+                <div className="text-[#FAFAFA] text-sm">
+                    <p>&copy; 2026 KeenKeeper. All rights reserved.</p>
+                </div>
+                <div className="text-[#FAFAFA] flex gap-8 text-sm">
+                    <a href="">Privacy Policy</a>
+                    <a href="">Terms of Service</a>
+                    <a href="">Cookies</a>
+                </div>
+            </div>
         </div>
     );
 };
